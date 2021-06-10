f06c82aabc03e170b5fa8367b00d3b87c9542039
pablorosflores <pablo.ros.flores@gmail.com>
2021-06-10 23:23:18 +0200

    lazy load

--- public/serendipia.js
+++ public/serendipia.js
@@ -1,12 +1,11 @@
-import images from '../img/*.jpg';
-
 let imgChange = document.getElementById("myImg");
 
+
+
 imgChange.addEventListener("mouseover", () => {
-  console.log(images);
-  //document.getElementById("toChange").srcset = require("../img/manon2.jpg");
+  document.getElementById("toChange").srcset = require("../img/manon2.jpg");
 });
 
 imgChange.addEventListener("mouseout", () => {
-  console.log(images);
+  document.getElementById("toChange").srcset = require("../img/manon.jpg");
 });
